817e8d8d3faf5ac4968d5996ffcc6e8e00bef90d
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-20 21:51:38 +1000

    feat: Added test app.

---
+++ src/apps/H.Hooks.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using H.Hooks;
+using H.Tests.Extensions;
+
+using var hook = new LowLevelKeyboardHook
+{
+    IsCapsLock = true,
+    Handling = true,
+    IsLeftRightGranularity = true,
+    IsExtendedMode = true,
+    HandleModifierKeys = true,
+}.WithEventLogging();
+
+hook.Start();
+
+await Task.Delay(TimeSpan.FromHours(5));
